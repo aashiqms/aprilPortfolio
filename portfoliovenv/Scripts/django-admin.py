fd669253d0127e0455213a8cdc4e35d69602a590
Aashiq MS <aashiqms@outlook.com>
2022-04-14 00:53:30 -0700

    initial commit

---
+++ portfoliovenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\aashi\personal\personal_portfolio\portfoliovenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ static/js/script.js
@@ -0,0 +1,71 @@
+
+// Django built in js function for handling csrf request to api's
+
+		function getCookie(name) {
+		    let cookieValue = null;
+		    if (document.cookie && document.cookie !== '') {
+		        let cookies = document.cookie.split(';');
+		        for (let i = 0; i < cookies.length; i++) {
+		            let cookie = cookies[i].trim();
+		            // Does this cookie string begin with the name we want?
+		            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+		                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+		                break;
+		            }
+		        }
+		    }
+		    return cookieValue;
+		}
+		let csrftoken = getCookie('csrftoken');
+
+// .....................................................................................................................
+
+		let activeItem = null
+		let list_snapshot = []
+
+// .....................................................................................................................
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+// Function to display all projects
+
+buildList()
+
+		function buildList(){
+
+			let url = '/api/project-list/';
+
+			fetch(url)
+			.then((resp) => resp.json())
+			.then(function(data){
+				console.log('Data:', data)
+
+				data.forEach((element, i) => {
+
+					let details = `
+					                <p><span class="heading-green">Project Name: </span></p>
+									<p>${element.title}</p>
+									<p><span class="heading-green">Description: </span></p>
+									<p>${element.description}</p>
+									<p><span class="heading-green">Source Code: </span></p>
+									<p>
+									<a href="${element.project_url}" style="text-decoration: none;
+									color: orange">${element.project_url}</a></p>
+									<hr>
+                                 `
+					console.log('element', element)
+					let item = `
+						<li class="item" style="color: white;list-style: none;margin-right: 10px;margin-left: 10px">
+          ${details}
+      </li>
+</div>
+					`
+                    let items = document.getElementById('items')
+                    items.innerHTML += item
+                    console.log(item)
+				})
+
+
+
+			})
+		}
